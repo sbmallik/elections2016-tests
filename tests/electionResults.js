bdee8946fffb815a4eb716493a08c1e2c29e2872
sbmallik <smallik@gannett.com>
2016-10-27 14:03:01 -0400

    Removed the sample tests, defined constants at one place.

--- tests/electionResults.js
+++ tests/electionResults.js
@@ -1,15 +1,17 @@
+require('../lib/constants.js');
+
 module.exports = {
   'disabled': false,
   'tags': ['elections2016', 'president', 'senate', 'house', 'governor'],
 
   'before': function (client) {
-    client.resizeWindow(1280, 1024)
+    client.resizeWindow(CONSTANTS.BROWSER_WIDTH, CONSTANTS.BROWSER_HEIGHT)
     electionResults = client.page.electionResults();
     electionResults.navigate(electionResults.url);
   },
 
   'Election Results - President': function (client) {
-    electionResults.waitForElementVisible('@logo', 1000),
+    electionResults.waitForElementVisible('@logo', CONSTANTS.TIMEOUT),
     electionResults.expect.element('@page_heading').text.to.equal('President Results');
     electionResults.expect.element('@dem_count').text.to.not.equal('0');
     electionResults.expect.element('@rep_count').text.to.not.equal('0');
@@ -17,7 +19,7 @@ module.exports = {
 
   'Election Results - Senate': function (client) {
     electionResults.click('@senate_link');
-    electionResults.waitForElementVisible('@logo', 1000),
+    electionResults.waitForElementVisible('@logo', CONSTANTS.TIMEOUT),
     electionResults.expect.element('@page_heading').text.to.equal('Senate Results');
     electionResults.expect.element('@dem_count').text.to.not.equal('0');
     electionResults.expect.element('@rep_count').text.to.not.equal('0');
@@ -25,7 +27,7 @@ module.exports = {
     
   'Election Results - House': function (client) {
     electionResults.click('@house_link');
-    electionResults.waitForElementVisible('@logo', 1000),
+    electionResults.waitForElementVisible('@logo', CONSTANTS.TIMEOUT),
     electionResults.expect.element('@page_heading').text.to.equal('House Results');
     electionResults.expect.element('@dem_count').text.to.not.equal('0');
     electionResults.expect.element('@rep_count').text.to.not.equal('0');
@@ -33,7 +35,7 @@ module.exports = {
     
   'Election Results - Governor': function (client) {
     electionResults.click('@governor_link');
-    electionResults.waitForElementVisible('@logo', 1000),
+    electionResults.waitForElementVisible('@logo', CONSTANTS.TIMEOUT),
     electionResults.expect.element('@page_heading').text.to.equal('Governor Results');
     electionResults.expect.element('@dem_count').text.to.not.equal('0');
     electionResults.expect.element('@rep_count').text.to.not.equal('0');
